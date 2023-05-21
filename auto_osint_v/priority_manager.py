32bcd35fdd67b2ab4b6607cc23c537344e22c0cd
Adam Cvjetan <up2040499@myport.ac.uk>
2023-05-21 19:26:29 +0100

    add --no-sandbox argument for running as root user in colab
    Took 5 minutes

--- auto_osint_v/priority_manager.py
+++ auto_osint_v/priority_manager.py
@@ -106,8 +106,8 @@ class PriorityManager:
             options = webdriver.ChromeOptions()
             options.headless = True
             options.add_argument("start-maximized")
+            options.add_argument("--no-sandbox")
             options.add_experimental_option("excludeSwitches", ["enable-automation"])
-            options.add_experimental_option('useAutomationExtension', False)
             options.add_argument('--disable-blink-features=AutomationControlled')
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
